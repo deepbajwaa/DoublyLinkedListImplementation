e4a7e64fa157c54749158e0c1d1b071bf324d7c8
Simrandeep Bajwa <sbajwa05@fred.socs.uoguelph.ca>
2019-12-21 14:36:00 -0500

    Finished working on the append function, now for some testing.

--- doublyList.c
+++ doublyList.c
@@ -1,1 +1,34 @@
 #include "doublyList.h"
+
+void append(struct node **head, int data)
+{
+  /*Declare a single pointer in order to traverse through the list*/
+  struct node *traverse = *head;
+
+  /*Initialize a new node*/
+  struct node *newNode = malloc(sizeof(struct node));
+
+  /*Initialize with data*/
+  newNode->data = data;
+  newNode->next = NULL;
+  newNode->prev = NULL;
+
+  /*Check to see if head exists, if it does then append the new node as the head of the list*/
+  if(*head)
+  {
+    *head = newNode;
+    return;
+  }
+  else
+  {
+    /*Move to the end of the list*/
+    while(traverse->next != NULL)
+    {
+      traverse = traverse->next;
+    }
+
+    /*Append the new node to the end of the list*/
+    traverse->next = newNode;
+    newNode->prev = traverse;
+  }
+}
